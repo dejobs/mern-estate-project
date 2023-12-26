d2a99e5408b10238c02f1133716bf74adb466a07
Adedeji Bisade <dejobs247@yahoo.com>
2023-12-26 01:30:29 +0100

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,12 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => console.log("Connected live to mongoDB server!")).catch((err) => console.log(err))
 
 const app = express();
 
 app.listen(3005, () => {
-    console.log("server is running on port 3005. Thank you")
-})
+  console.log("server is running on port 3005. Thank you");
+});
