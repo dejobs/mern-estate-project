c9df9ab06751addbe2f2f0623d47c1cf87096a34
Adedeji Bisade <dejobs247@yahoo.com>
2024-01-02 02:53:19 +0100

    add redux toolkit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 
 
 export default function Home() {
+  const {currentUser} = useSelector(state => state.user)
   return (
     <div>
       <h1 className="text-3xl font-bold underline text-red-500 m-10 bg-green-400 border">
         Hello world!
       </h1>
+      <h3>Welcome {currentUser?.username} </h3>
       
      
     </div>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux"; 
+import {signInstart, signInSuccess, signInFailure} from "../redux/user/userSlice"
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading, error} = useSelector(state => state.user)
   const navigate = useNavigate()
+  const dispatch = useDispatch()
  
   
 
@@ -20,7 +22,7 @@ export default function SignUp() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      setLoading(true)
+      dispatch(signInstart())
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -31,19 +33,14 @@ export default function SignUp() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setError(data.message);
-        setLoading(false);
+        dispatch(signInFailure(data.message))
         return
       }
-      setError(null)
+      dispatch(signInSuccess(data))
       navigate("/")
     } catch (err) {
-      setError(err.message)
-      setLoading(false)
-    } finally {
-      setLoading(false)
-    }
-    
+      dispatch(signInFailure(err.message))
+    } 
   };
   
 
