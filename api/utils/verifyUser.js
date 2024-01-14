b4cc7089aeefcbebc4b8bd2bcbdc42685255d819
Adedeji Bisade <dejobs247@yahoo.com>
2024-01-14 17:57:18 +0100

    create user update api

---
+++ api/utils/verifyUser.js
@@ -0,0 +1,14 @@
+import { errorHandler } from "./error.js"
+import  Jwt  from "jsonwebtoken";
+
+export const verifyToken = (req, res, next) => {
+ const token = req.cookies.access_token 
+ if (!token) next(errorHandler(401, "Unauthorized"));
+
+Jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Forbidden"));
+
+    req.user = user;
+    next()
+} )
+}
