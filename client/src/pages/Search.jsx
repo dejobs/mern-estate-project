8146eec67a27ecfb5054d5377f0c677e9f23b71d
Adedeji Bisade <dejobs247@yahoo.com>
2024-01-27 14:16:53 +0100

    create search page ui

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div className="flex flex-col md:flex-row">
+      <div className="p-7 border-b md:border-r-2 md:min-h-screen">
+        <form className="flex flex-col gap-8">
+          <div className="flex items-center gap-2">
+            <label className="whitespace-nowrap font-semibold" htmlFor="searchTerm">
+              Search Term
+            </label>
+            <input
+              type="text"
+              id="serchTerm"
+              placeholder="Search..."
+              className="border rounded-lg p-3 w-full"
+            />
+          </div>
+          <div className="flex gap-2 items-center flex-wrap">
+            <label className="font-semibold">Type:</label>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="all" className="w-5" />
+              <span>Rent & Sale</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="rent" className="w-5" />
+              <span>Rent</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="sell" className="w-5" />
+              <span>Sale</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="offer" className="w-5" />
+              <span>Offer</span>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center flex-wrap">
+            <labe className="font-semibold">Amenities:</labe>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="parking" className="w-5" />
+              <span>Parking</span>
+            </div>
+            <div className="flex gap-2 items-center">
+              <input type="checkbox" id="furnished" className="w-5" />
+              <span>Furnished</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-2 ">
+            <label className="font-semibold">Sort:</label>
+            <select id="sort_order" className="border rounded-lg p-3">
+                <option>Price high to low</option>
+                <option>Price low to high</option>
+                <option>Latest</option>
+                <option>Oldest</option>
+            </select>
+          </div>
+          <button className="bg-slate-700 text-white border rounded-lg uppercase p-3">search</button>
+        </form>
+      </div>
+      <div>
+        <h1 className="text-3xl font-semibold border-b p-3 mt-6 text-slate-700">Listing results</h1>
+      </div>
+    </div>
+  );
+}
