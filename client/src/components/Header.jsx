d4f8f2eea840bf74fc40ecea4a9d41abcf54871d
Adedeji Bisade <dejobs247@yahoo.com>
2024-01-27 11:29:08 +0100

    complete header search form fnctionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,29 @@
 import { FaSearch } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate, useSearchParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 function Header() {
   const { currentUser } = useSelector((state) => state.user);
-  
+  const [searchTerm, setSearchTerm] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center mx-auto p-3">
@@ -14,13 +33,20 @@ function Header() {
             <span className="text-slate-700">Estate</span>
           </h1>
         </NavLink>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center ">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center "
+        >
           <input
             type="text"
             placeholder="search..."
             className="bg-transparent indent-2 rounded-md focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-slate-600" />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
           <NavLink to="/">
@@ -34,9 +60,12 @@ function Header() {
             </li>
           </NavLink>
           <NavLink to="/profile">
-            {currentUser? (
-              <img className="rounded-full h-7 w-7 object-cover"
-              src={currentUser.avatar} alt="profile" />
+            {currentUser ? (
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
             ) : (
               <li className="text-slate-700 hover:underline ">Sign in</li>
             )}
